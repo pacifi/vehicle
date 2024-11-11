0cfb36340b7de4d79cd1bae8c464f25cdba601ad
pacifi <pacifi.bnr@gmail.com>
2024-11-11 09:43:12 -0500

    grafico iniciado

--- applica/apis.py
+++ applica/apis.py
@@ -1,9 +1,33 @@
+from django.db.models import Count
+from django.db.models.functions import TruncMonth
 from rest_framework import viewsets
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-from applica.models import Conductor
+from applica.models import Conductor, ExcesoVelocidad
 from applica.serializers import ConductorSerializer
 
 
-class ConductorModelViewSet(viewsets.ModelViewSet):
-    queryset = Conductor.objects.all()
-    serializer_class = ConductorSerializer
+class ExcesoVelocidadPorMesAPIView(APIView):
+    def get(self, request, year=2024):
+        # Filtrar por año usando `fecha_evento__year`
+        registros = (
+            ExcesoVelocidad.objects
+            .filter(fecha_evento__year=year)
+            .annotate(mes=TruncMonth('fecha_evento'))
+            .values('mes')
+            .annotate(cantidad=Count('id'))
+            .order_by('mes')
+        )
+
+        # Crear un diccionario para contar registros por cada mes del año
+        conteo_mensual = {mes: 0 for mes in range(1, 13)}
+
+        for registro in registros:
+            mes = registro['mes'].month
+            conteo_mensual[mes] = registro['cantidad']
+
+        # Convertir el conteo a una lista en el formato deseado
+        respuesta = [conteo_mensual[mes] for mes in range(1, 13)]
+
+        return Response(respuesta)
--- applica/urls.py
+++ applica/urls.py
@@ -1,18 +1,20 @@
 from django.urls import path
 from rest_framework import routers
 
-from applica.apis import ConductorModelViewSet
+from applica.apis import ExcesoVelocidadPorMesAPIView
 from applica.views import reporte_velocidad
 
 router = routers.SimpleRouter()
 
-router.register("conductors", ConductorModelViewSet)
+
 
 app_name = "applica"
 
 urlpatterns = [
 
     path('reporte/velocidad', reporte_velocidad, name='reporte_velocidad'),
+    path('api/excesos-velocidad/<int:year>/', ExcesoVelocidadPorMesAPIView.as_view(), name='excesos-velocidad-por-mes'),
+
 ]
 
 urlpatterns = urlpatterns + router.urls
